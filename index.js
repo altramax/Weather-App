baf4334cce04ee52e61b2bb2620bfebbdb33ce3d
Ezekiel Ajayi <ajayiezekiel559@gmail.com>
2023-02-13 18:11:25 +0100

    Time zone Update

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ futureWeathercast.innerHTML = Forcast;
   let {longitude, latitude} = success.coords
 
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${ApiKey}`)
-  .then(res => res.json().then(data =>{console.log(data); getWeather(data)}))  
+  .then(res => res.json().then(data => getWeather(data)))  
 })
 
 
